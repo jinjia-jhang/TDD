66585d5ab33e1367819a89400f2c8362b61ebf51
Jin-Jia Jhang <last810112@gmail.com>
2022-04-18 23:20:29 +0800

    整除5與包含5抽方法, 整除7與包含7抽方法

--- Coding-DOJO/CalculateService.cs
+++ Coding-DOJO/CalculateService.cs
@@ -4,20 +4,30 @@ public class CalculateService
 {
     public string ReplaceNumber(int number)
     {
-        if (ContainsAndDivide_3(number) &&
-            (number % 5 == 0 || number.ToString().Contains("5")) &&
-            (number % 7 == 0 || number.ToString().Contains("7")))
-        {
-           return "FizzBuzzWhizz";
-        }
-        if ((number % 3 == 0 && number % 5 == 0) || (number.ToString().Contains("3") && number.ToString().Contains("5"))) return "FizzBuzz";
+        if (ContainsAndDivide_3(number) && ContainsAndDivide_5(number) && ContainsAndDivide_7(number))
+            return "FizzBuzzWhizz";
+        
+        if ((number % 3 == 0 && number % 5 == 0) || (number.ToString().Contains("3") && number.ToString().Contains("5"))) 
+            return "FizzBuzz";
+        
         if (ContainsAndDivide_3(number)  && (number % 7 == 0 || number.ToString().Contains("7"))) 
             return "FizzWhizz";
+        
         if (ContainsAndDivide_3(number)) return "Fizz";
-        if (number % 5 == 0 || number.ToString().Contains("5")) return "Buzz";
-        if (number % 7 == 0 || number.ToString().Contains("7")) return "Whizz";    
+        if (ContainsAndDivide_5(number) ) return "Buzz";
+        if (ContainsAndDivide_7(number)) return "Whizz";    
         
         return number.ToString();
+    }
+
+    private static bool ContainsAndDivide_7(int number)
+    {
+        return number % 7 == 0 || number.ToString().Contains("7");
+    }
+
+    private static bool ContainsAndDivide_5(int number)
+    {
+        return (number % 5 == 0 || number.ToString().Contains("5"));
     }
 
     private static bool ContainsAndDivide_3(int number)
