bff9e8279f0fff0153dc0a5472dd1ec3ce51e0aa
Jin-Jia Jhang <last810112@gmail.com>
2022-04-18 22:35:52 +0800

    加入input 5的測試

--- Coding-DOJO.Tests/UnitTest1.cs
+++ Coding-DOJO.Tests/UnitTest1.cs
@@ -4,18 +4,28 @@ namespace Coding_DOJO.Tests;
 
 public class Tests
 {
+    private CalculateService _calculateService;
     [SetUp]
     public void Setup()
     {
+        _calculateService = new CalculateService();
     }
 
     [Test]
     public void methodsName_input3_Fizz()
     {
         var number = 3;
-
-        var actual = new CalculateService().ReplaceNumber(number);
+        var actual = _calculateService.ReplaceNumber(number);
 
         Assert.AreEqual("Fizz", actual);
     }
+    [Test]
+    public void methodsName_input5_Buzz()
+    {
+        var number = 5;
+        var actual = _calculateService.ReplaceNumber(number);
+
+        Assert.AreEqual("Buzz", actual);
+    }
+    
 }
--- Coding-DOJO/CalculateService.cs
+++ Coding-DOJO/CalculateService.cs
@@ -5,6 +5,7 @@ public class CalculateService
     public string ReplaceNumber(int number)
     {
         if (number % 3 == 0) return "Fizz";
+        if (number % 5 == 0) return "Buzz";
         
         return number.ToString();
     }
