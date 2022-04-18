37a5cbf0fb591a5d13f7d4f73f11dc2a0a7f77cf
Jin-Jia Jhang <last810112@gmail.com>
2022-04-18 22:18:36 +0800

    TDD sln 建置, Coding-DOJO prj 建置

---
+++ Coding-DOJO/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Coding_DOJO;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
