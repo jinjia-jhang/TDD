aad40ffb2597ee7c278d2a66319908886b62f15b
Jin-Jia Jhang <last810112@gmail.com>
2022-04-18 23:29:57 +0800

    抽離整除邏輯

--- Coding-DOJO/CalculateService.cs
+++ Coding-DOJO/CalculateService.cs
@@ -5,14 +5,12 @@ public class CalculateService
     public string ReplaceNumber(int number)
     {
         if (ContainsAndDivide_3(number) && ContainsAndDivide_5(number) && ContainsAndDivide_7(number))
-            return "FizzBuzzWhizz";
+        if (ContainsAndDivide_3(number) && ContainsAndDivide_5(number) && ContainsAndDivide_7(number))
+           return "FizzBuzzWhizz";
         
-        if ((number % 3 == 0 && number % 5 == 0) || (number.ToString().Contains("3") && number.ToString().Contains("5"))) 
-            return "FizzBuzz";
-        
+        if ((number % 3 == 0 && number % 5 == 0) || (number.ToString().Contains("3") && number.ToString().Contains("5"))) return "FizzBuzz";
         if (ContainsAndDivide_3(number)  && (number % 7 == 0 || number.ToString().Contains("7"))) 
             return "FizzWhizz";
-        
         if (ContainsAndDivide_3(number)) return "Fizz";
         if (ContainsAndDivide_5(number) ) return "Buzz";
         if (ContainsAndDivide_7(number)) return "Whizz";    
@@ -22,16 +20,24 @@ public class CalculateService
 
     private static bool ContainsAndDivide_7(int number)
     {
-        return number % 7 == 0 || number.ToString().Contains("7");
+        var divisionNum = 7;
+        return (IsDevideNumber(number, divisionNum) || number.ToString().Contains(divisionNum.ToString()));
     }
 
     private static bool ContainsAndDivide_5(int number)
     {
-        return (number % 5 == 0 || number.ToString().Contains("5"));
+        var divisionNum = 5;
+        return (IsDevideNumber(number, divisionNum) || number.ToString().Contains(divisionNum.ToString()));
     }
 
     private static bool ContainsAndDivide_3(int number)
     {
-        return number % 3 == 0 || number.ToString().Contains("3");
+        var divisionNum = 3;
+        return IsDevideNumber(number, divisionNum) || number.ToString().Contains(divisionNum.ToString());
+    }
+
+    private static bool IsDevideNumber(int number, int divisionNum)
+    {
+        return number % divisionNum == 0;
     }
 }
